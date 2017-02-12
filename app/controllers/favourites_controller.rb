1c3c228c06354d9b4ef80d85383e3fb6c93014b2
VidyullathaKandipati <latha522@gmail.com>
2017-02-12 13:56:00 +1100

    Updated secrets.yml to have secret base

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,14 +1,19 @@
 class FavouritesController < ApplicationController
+  # before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @favourites = Favourite.all
   end
 
   def show
   end
 
   def new
+    @favourite = Favourite.new
   end
 
   def create
+    # @favourite = Favourite.create favourite_params
   end
 
   def edit
