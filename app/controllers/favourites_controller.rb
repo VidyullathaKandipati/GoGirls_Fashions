ad86aa51289cb348ca1794a0864c5557a612df2f
VidyullathaKandipati <latha522@gmail.com>
2017-02-13 10:48:17 +1100

    Product review and favourites are completed

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,12 +3,13 @@ class FavouritesController < ApplicationController
 
   def create
     # @product = Product.find params[:product_id]
-    @favourite = Favourite.create( product_id: params[:product_id], user_id: params[:user_id] )
+    favourite = Favourite.create( product_id: params[:product_id], user_id:@current_user.id)
     redirect_to product_path(params[:product_id])
   end
 
   def destroy
-    favourite = Favourite.find params[:id]
+    favourite = Favourite.find_by(user_id: @current_user.id, product_id: params[:product_id])
     favourite.destroy
+    redirect_to product_path(params[:product_id])
   end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -12,4 +12,8 @@
 class Favourite < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  def does_user_already_likes_product
+    errors.add(:user, "You can like product only once.") if (user.already_likes?(product))
+  end
 end
