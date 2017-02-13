d87c44d095afcdb9a8154e70d52184208e6db39d
VidyullathaKandipati <latha522@gmail.com>
2017-02-14 10:41:10 +1100

    Some randomn changes, will bereverted back.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :fetch_user
+  # before_action :set_order
 
   private
   def fetch_user
