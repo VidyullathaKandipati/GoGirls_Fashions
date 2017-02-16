b37acee59b3381f3e73f5a0038d763ccf67af5b2
Jessica Tran <kryx11@gmail.com>
2017-02-16 15:28:10 +1100

    updates cart and payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,11 +4,16 @@ class ChargesController < ApplicationController
   def new
     @order = Order.find params[:order_id]
     @amount = @order.total_cost
+
+    if @amount == 0
+      redirect_to :back
+    end
   end
 
   def create
     @order = Order.find params[:order_id]
     @amount = @order.total_cost*100
+
     @email = @current_user.email
 
     customer = Stripe::Customer.create(
@@ -22,26 +27,16 @@ class ChargesController < ApplicationController
       :description => 'Fashion brand customer',
       :currency    => 'aud'
     )
+    redirect_to order_charges_path
 
-
-
-  redirect_to order_charges_path
-  # render :destroy
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
      redirect_to new_order_charge_path
+
+
    end
 
-  #  def destroy
-  #    @order = Order.find params[:order_id]
-  #    @order.destroy if @order.id == session[:order_id]
-  #    session[:order_id] = nil
-   #
-  #    respond_to do |format|
-  #      format.html {redirect_to order_charges_path, notice: 'Your cart is currently empty'}
-  #    end
-  #  end
 
 def index
   # @order = Order.find params[:order_id]
@@ -51,6 +46,7 @@ def index
   @order.status = true
   @order.save
   session[:order_id] = nil
+
   redirect_to thanks_path
 end
 
