7df80477e56099fbca6d6b522401da8092083561
Naomi O'Sullivan <osullivan.naomi@gmail.com>
2017-02-16 17:36:41 +1100

    seed data women

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,22 @@ u4 = User.create(name: 'Naomi', email: 'naomi@ga.com', password: 'chicken', admi
 
 Product.destroy_all
 #Female products
-p1 = Product.create(name: 'Floral printed wrap dress', cost: 30, size: 's', color: '', gender: 'women', brand:'Vero Moda', info: "Floral printed wrap dress");
-p2 = Product.create(name: 'Midi Skirt', cost: 75, size: 's', color: '', gender: 'women', brand:'Vero Moda', info: "Midi Skirt in Satin with Splices in Print");
-p9 = Product.create(name: 'Pyjama Shirt Dress', cost: 69, size: 's', color: 'black', gender: 'women', brand:'Monki', info: "Pyjama Shirt Dress with white piping");
-p10 = Product.create(name: 'Wide Leg Culottes with Frill Hem', cost: 69, size: 's', color: 'black', gender: 'women', brand:'Neon Rose', info: "Smooth woven frabric, mid rise waist");
-p11 = Product.create(name: 'Metallic Pleated Midi Skirt', cost: 52, size: 's', color: 'silver', gender: 'women', brand:'Pimkie', info: "Semi-sheer fabric, high waist");
-p12 = Product.create(name: 'Floral embroidered Bomber Jacket', cost: 459, size: 'm', color: 'floral', gender: 'women', brand:'Needle and Thread', info: "Fully lined, zip fastening");
+p1 = Product.create(name: 'Floral printed wrap dress', cost: 30, size: 's', color: '', gender: 'women', brand:'Vero Moda', info: "Floral printed wrap dress.");
+p2 = Product.create(name: 'Midi Skirt', cost: 75, size: 's', color: '', gender: 'women', brand:'Vero Moda', info: "Midi Skirt in Satin with Splices in Print.");
+p9 = Product.create(name: 'Pyjama Shirt Dress', cost: 69, size: 's', color: 'black', gender: 'women', brand:'Monki', info: "Pyjama Shirt Dress with white piping.");
+p10 = Product.create(name: 'Wide Leg Culottes with Frill Hem.', cost: 69, size: 's', color: 'black', gender: 'women', brand:'Neon Rose', info: "Smooth woven frabric, mid rise waist");
+p11 = Product.create(name: 'Metallic Pleated Midi Skirt.', cost: 52, size: 's', color: 'silver', gender: 'women', brand:'Pimkie', info: "Semi-sheer fabric, high waist.");
+p12 = Product.create(name: 'Floral embroidered Bomber Jacket', cost: 459, size: 'm', color: 'floral', gender: 'women', brand:'Needle and Thread', info: "Fully lined, zip fastening.");
+p13 = Product.create(name: 'Joshua Floral Mini Dress', cost: 229, size: 'm', color: 'navy/floral', gender: 'women', brand:'Oxford', info: "Mock turtle neck and flattering fit.");
+p14 = Product.create(name: 'Bella Velvet Dress', cost: 140, size: 's', color: 'black', gender: 'women', brand:'Bardot', info: "Off-the-shoulder neckline and tactile velvet gives this piece a timeless appeal.");
+p15 = Product.create(name: 'Soul Silence Dress', cost: 119, size: 'l', color: 'ash', gender: 'women', brand:'One Teaspoon', info: "Comfy and relaxed fit with frilled cuffs and hemline. Deep v-neck and tie detail on the neck.");
+p16 = Product.create(name: 'Dado Skirt', cost: 500, size: 's', color: 'silver', gender: 'women', brand:'Max & Co', info: "A-lined skirt crafted from silver laminated leather, it will bring a futuristic element to your wardrobe.");
+p17 = Product.create(name: 'Wonder Lace-Up Skirt', cost: 170, size: 'm', color: 'black', gender: 'women', brand:'Ministry of Style', info: "The cotton-linen skirt is designed for summer, keeping you cool and comfortable, while the eyelet lace-up detail makes a statement.");
+p18 = Product.create(name: 'LS Scoop Bodysuit', cost: 40, size: 'l', color: 'nude', gender: 'women', brand:'Miss Selfridge', info: "Ideal for layering, made from a super-soft stretch jersey. Wear it under skirts or high-waisted pants for a clean, no-fuss outfit.");
+p19 = Product.create(name: 'Boxy Tee', cost: 40, size: 'l', color: 'stripe', gender: 'women', brand:'Assembly Label', info: "Cut for a relaxed fit, this short-sleeved t-shirt is made from breathable blend of linen and cotton for complete softness and a relaxed drape.");
+p20 = Product.create(name: 'Esmerelda Wrap Top', cost: 60, size: 'm', color: 'gingham', gender: 'women', brand:'Lioness', info: "Billowing gathered sleeves that make maximum impact, this top is perfect for dressing up any look. Style yours with distressed denim to balance the feminine silhouette or play on the delicate look by pairing the top with an A-line mini skirt.");
+
+
 
 
 #Male Products
